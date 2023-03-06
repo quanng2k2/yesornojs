78591c936a450b736463c0f57094cf1c6c448207
unknown <nguuyenngocquan2002hh@gmail.com>
2023-03-06 13:59:06 +0700

    add file

---
+++ main.js
@@ -0,0 +1,9 @@
+function yesButton() {
+    document.write("I love tou too");
+}
+function noHover() {
+    let x = Math.round(Math.random() * 500);
+    let y = Math.round(Math.random() * 500);
+    document.getElementById("ip11").style.left = x + 'px';
+    document.getElementById("ip11").style.top = y + 'px';
+}
